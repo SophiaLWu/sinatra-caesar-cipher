27eecbe01ba4165a2fd468a471ac423682de79a1
Sophia <swoozie14.com>
2016-12-12 18:08:53 -0800

    Add gemfile

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require "sinatra/reloader"
+#require "sinatra/reloader"
 
 # Takes a string and shift factor and outputs the Caesar ciphered string
 def caesar_cipher(str, shift_factor)
